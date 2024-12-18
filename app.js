8faa3fcea950fc7527513a03371c6a95cbbb48a0
Prasanna Gaikwad <prasannagaikwad0018@gmail.com>
2024-12-18 20:31:06 +0530

    cors

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(cookieParser())
 app.use(cors({
     origin: 'https://blogverse-frontend.onrender.com', // Frontend URL
     credentials: true, // Allow credentials
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }))
 
 
